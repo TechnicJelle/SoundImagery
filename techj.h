029f7ebaa734c0986a3a612be97dd53967d31616
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-05-01 03:43:45 +0200

    new techj.h update

--- techj.h
+++ techj.h
@@ -1,5 +1,14 @@
-//version 001
+//version 002
+
+// import for print
 #include <stdio.h>
+
+// import for sleep
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
 void printi(int num) {
 	printf("%d\n", num);
@@ -7,4 +16,12 @@ void printi(int num) {
 
 void prints(char string[]) {
 	printf("%s\n", string);
+}
+
+void meSleep(int sec) {
+	#ifdef _WIN32
+	Sleep(sec*1000);
+	#else
+	sleep(sec);
+	#endif
 }
